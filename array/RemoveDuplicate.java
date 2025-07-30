fbd8585f8397ff5bb145f784a8f8536c5b83dfc6
TANAYCHORDIA <tanayhc24@gmail.com>
2025-07-30 16:01:04 +0530

    feat: Remove duplicates from a Sorted Array inplace.

---
+++ array/RemoveDuplicate.java
@@ -0,0 +1,16 @@
+package array;
+
+public class RemoveDuplicate {
+    public static void main(String args[]){
+        int arr[]= {0,0,1,1,1,2,2,3,3,4};
+        int n = arr.length;
+        int i =0;
+        for(int j=1;j<n;j++){
+            if(arr[i]!=arr[j]){
+                i++;
+                arr[i]=arr[j];
+            }
+        }
+        System.out.println("Number of unique elements:"+(i+1));
+    }
+}
