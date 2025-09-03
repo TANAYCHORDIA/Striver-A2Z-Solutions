37610788d883e6bbe19728e2b909883e946b33d6
TANAYCHORDIA <tanayhc24@gmail.com>
2025-09-03 22:15:01 +0530

    feat: Implement optimal Merge Sort solution for Reverse Pairs.

---
+++ array/reversePairs.java
@@ -0,0 +1,61 @@
+package array;
+
+import java.util.ArrayList;
+
+public class reversePairs {
+    public static void main(String[] args) {
+        int arr[] = {2,4,3,5,1};
+        System.out.println(reversePair(arr));
+    }
+    public static int reversePair(int[] nums) {
+        int n = nums.length;
+        return ms(nums,0,n-1);
+    }
+    public static int merge(int arr[],int low,int mid,int high){
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = low;
+        int right = mid+1;
+        int cnt = 0;
+        while(left<=mid && right<=high){
+            if(arr[left]>=arr[right]){
+                temp.add(arr[right]);
+                right++;
+            }
+            else{
+                temp.add(arr[left]);
+                left++;
+            }
+        }
+        while(left<=mid){
+            temp.add(arr[left]);
+            left++;
+        }
+        while(right<=high){
+            temp.add(arr[right]);
+            right++;
+        }
+        for(int i = low;i<=high;i++){
+            arr[i] = temp.get(i-low);
+        }
+        return cnt;
+    }
+    public static int countPairs(int[] arr, int low, int mid, int high) {
+        int right = mid + 1;
+        int cnt = 0;
+        for (int i = low; i <= mid; i++) {
+            while (right <= high && arr[i] > 2L * arr[right]) right++;
+            cnt += (right - (mid + 1));
+        }
+        return cnt;
+    }
+    public static int ms(int arr[],int low,int high){
+        int cnt=0;
+        if(low>=high)return cnt;
+        int mid = (low+high)/2;
+        cnt+=ms(arr,low,mid);
+        cnt+=ms(arr,mid+1,high);
+        cnt+=countPairs(arr,low,mid,high);
+        merge(arr,low,mid,high);
+        return cnt;
+    }
+}
