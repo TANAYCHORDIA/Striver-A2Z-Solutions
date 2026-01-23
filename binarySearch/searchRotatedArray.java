c7a49b2865bed41b159e9813d8c920873be583d9
TANAYCHORDIA <tanayhc24@gmail.com>
2026-01-23 20:27:59 +0530

    feat: Solve Search in Rotated Sorted Array I

---
+++ binarySearch/searchRotatedArray.java
@@ -0,0 +1,30 @@
+package binarySearch;
+
+public class searchRotatedArray {
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int n = nums.length;
+        int high = n-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]<=nums[high]){
+                if(nums[mid]<=target && nums[high]>=target){
+                    low = mid+1;
+                }
+                else high = mid-1;
+                
+            }
+            else if(nums[mid]>nums[high]){
+                if(target<=nums[mid] && target>=nums[low])high = mid-1;
+                else low = mid+1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 0;
+        System.out.println(search(nums,target));
+    }
+}
