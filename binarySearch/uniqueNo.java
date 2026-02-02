e0da8902caf51c70f78f75763e3031804128abe3
TANAYCHORDIA <tanayhc24@gmail.com>
2026-02-02 13:51:48 +0530

    feat: Solve Single Element in Sorted Array

---
+++ binarySearch/uniqueNo.java
@@ -0,0 +1,26 @@
+package binarySearch;
+
+public class uniqueNo {
+    public static int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        if(n==1 || nums[0]!=nums[1])return nums[0];
+        if(nums[n-1]!=nums[n-2])return nums[n-1];
+        int low = 0;
+        int high = n-1;
+        while(low<=high){
+            int mid = low + (high -low)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])return nums[mid];
+            else if(nums[mid]==nums[mid+1] || nums[mid]==nums[mid-1]){
+                if(mid%2==0 && nums[mid]==nums[mid+1])low = mid+1;
+                else if(mid%2!=0 && nums[mid]==nums[mid+1])high = mid-1;
+                else if(mid%2==0 && nums[mid]==nums[mid-1])high=mid-1;
+                else low = mid+1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,3,3,4,4,8,8};
+        System.out.println(singleNonDuplicate(nums));
+    }
+}
