16b2e027039eea6ca7f4419051a871ba7c13c1e2
TANAYCHORDIA <tanayhc24@gmail.com>
2026-06-23 12:37:14 +0530

    feat: add Node class for binary tree implementation

---
+++ BinaryTrees/Node.java
@@ -0,0 +1,12 @@
+package BinaryTrees;
+public class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+} 
