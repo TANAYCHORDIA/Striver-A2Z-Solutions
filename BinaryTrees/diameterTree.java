552451d404679d832abdcdd8e0f7cb1c2bc9872d
TANAYCHORDIA <tanayhc24@gmail.com>
2026-06-29 11:11:36 +0530

    feat: implement diameter calculation for binary tree

---
+++ BinaryTrees/diameterTree.java
@@ -0,0 +1,27 @@
+package BinaryTrees;
+
+import javax.swing.tree.TreeNode;
+
+public class diameterTree {
+    static int val = 0;
+    public static int diameterOfBinaryTree(Node root) {
+        Height(root);
+        return val;
+    }
+    public static int Height(Node root){
+        if(root==null)return 0;
+        int l = Height(root.left);
+        int r = Height(root.right);
+        val = Math.max(val,l+r);
+        return 1+Math.max(l,r);
+    }
+    public static void main(String[] arg){
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        int ans = diameterOfBinaryTree(root);
+        System.out.println(ans);
+    }
+}
